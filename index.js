4f1d93809d9f4063c3f28a3a5e0630cf4954a525
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-09-16 18:55:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+  const test = "Testing";
+   console.log("This works!");
+
+  return test;
+   console.log("This works!");
+}
+
+const call = padRow("CamperChan");
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
